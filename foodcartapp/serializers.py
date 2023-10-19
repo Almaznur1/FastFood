89cd935814c013c1838964c012e9ae7653e90ad0
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-10-19 17:47:03 +0300

    add phonenumber serializer field to OrderSerializer

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer, ListField
+from phonenumber_field.serializerfields import PhoneNumberField
 from .models import Order, OrderItem
 
 
@@ -22,6 +23,7 @@ class OrderSerializer(ModelSerializer):
         allow_empty=False,
         write_only=True
     )
+    phonenumber = PhoneNumberField()
 
     class Meta:
         model = Order
